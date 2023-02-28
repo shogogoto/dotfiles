f30ca26485a09005607072cfdeff6e2606bab6bd
Shogo Goto <gotoadmn0605@gmail.com>
2023-02-28 22:02:26 +0900

    add vim-plug, tig, node, python installation

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# install neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install fromdos, todos command
+sudo apt install tofrodos
+
+# WSL2でクリップボードを有効にする
+sudo apt install xsel xclip
+
+sudo apt install tig
+
+# node
+sudo apt install npm
+
+# python
+sudo apt install python3-pip
+pip install pipenv
+curl https://pyenv.run | bash
