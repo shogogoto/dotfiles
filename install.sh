befb54447a9268aed54e285a43adfcef303d440a
Shogo Goto <gotoadmn0605@gmail.com>
2023-09-09 11:40:32 +0900

    add apt-fast

--- install.sh
+++ install.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:apt-fast/stable
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo apt update -y
 
+sudo apt install apt-fast -y
 # for ssh-agent
-sudo apt install keychain -y
+sudo apt-fast install keychain -y
 
 # space cli
-sudo apt install zip unzip
+sudo apt-fast install zip unzip
 curl -fsSL https://get.deta.dev/space-cli.sh | sh
 
-sudo apt install tofrodos -y # install fromdos, todos command
-sudo apt install xsel xclip -y # WSL2でクリップボードを有効にする
-sudo apt install tig -y
+sudo apt-fast install tofrodos -y # install fromdos, todos command
+sudo apt-fast install xsel xclip -y # WSL2でクリップボードを有効にする
+sudo apt-fast install tig -y
 
 # nodejs
-sudo apt install npm -y # node
+sudo apt-fast install npm -y # node
 sudo npm install -g n
 sudo n stable
-sodo apt purge nodejs npm -y # n導入後は不要なため
+sodo apt-fast purge nodejs npm -y # n導入後は不要なため
 
 # python
-sudo apt install libffi-dev -y # for resolve ModuleNotFoundError: No module named '_ctypes'
-sudo apt install libbz2-dev -y # for resoluve ModuleNotFoundError: No module named '_bz2' in networkx
-sudo apt install python3-pip -y
+sudo apt-fast install libffi-dev -y # for resolve ModuleNotFoundError: No module named '_ctypes'
+sudo apt-fast install libbz2-dev -y # for resoluve ModuleNotFoundError: No module named '_bz2' in networkx
+sudo apt-fast install python3-pip -y
 pip install pipenv
 curl https://pyenv.run | bash
 
 # docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt-fast install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 ## 権限設定 ref: https://linuxhandbook.com/docker-permission-denied/
 # ERROR: permission denied while trying to connect to the Docker daemon socket
 sudo groupadd docker
@@ -37,11 +39,11 @@ sudo usermod -aG docker $USER
 newgrp docker
 
 # etc
-sudo apt install eog -y # preview image file
-sudo apt install jq -y
+sudo apt-fast install eog -y # preview image file
+sudo apt-fast install jq -y
 
 # vim setup
-sudo apt install neovim -y
+sudo apt-fast install neovim -y
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugInstall -c q -c q
 
