ae0400993dd28a084253a6792aace7e4ddc48d11
Shogo Goto <gotoadmn0605@gmail.com>
2023-10-12 13:48:45 +0900

    poetry-version-plugin -> poetry-dynamic-versioning

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ pip install pipenv
 curl https://pyenv.run | bash
 curl -sSL https://install.python-poetry.org | python3 -
 poetry config virtualenvs.in-project true
-poetry self add poetry-version-plugin
+poetry self add "poetry-dynamic-versioning[plugin]"
 
 # docker
 sudo apt-fast install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
