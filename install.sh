3bdd1a50d216373a99c6d9045abbef3092697d68
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-26 23:56:06 +0900

    change install.sh

--- install.sh
+++ install.sh
@@ -2,56 +2,56 @@
 ./init_dots.sh # keychinに依存
 . ~/.bashrc
 
-sudo apt install software-properties-common -yqq # add-apt-repositoryを追加
-sudo add-apt-repository ppa:apt-fast/stable -y
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo apt update -yqq
-sudo apt upgrade -yqq
-sudo apt install apt-fast -yqq
+yes | sudo apt install software-properties-common -yq # add-apt-repositoryを追加
+yes | sudo add-apt-repository ppa:apt-fast/stable -y
+yes | sudo add-apt-repository ppa:neovim-ppa/unstable -y
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | yes | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+yes | sudo apt update -yq
+yes | sudo apt upgrade -yq
+sudo apt install apt-fast -y
 # for ssh-agent
-sudo apt-fast install keychain -yqq
+yes | sudo apt-fast install keychain -yq
 
 # space cli
-sudo apt-fast install zip unzip -yqq
+yes | sudo apt-fast install zip unzip -yq
 curl -fsSL https://get.deta.dev/space-cli.sh | sh
 
-sudo apt-fast install tofrodos -yqq # install fromdos, todos command
-sudo apt-fast install xsel xclip -yqq # WSL2でクリップボードを有効にする
-sudo apt-fast install tig -yqq
+yes | sudo apt-fast install tofrodos -yq # install fromdos, todos command
+yes | sudo apt-fast install xsel xclip -yq # WSL2でクリップボードを有効にする
+yes | sudo apt-fast install tig -yq
 
 # nodejs
-sudo apt-fast install npm -yqq # node
-sudo npm install -g n
-sudo n stable
-sodo apt-fast purge nodejs npm -yqq # n導入後は不要なため
+yes | sudo apt-fast install npm -yq # node
+yes | sudo npm install -g n
+yes | sudo n stable
+yes | sodo apt-fast purge nodejs npm -yq # n導入後は不要なため
 
 # python
-sudo apt-fast install libffi-dev -yqq # for resolve ModuleNotFoundError: No module named '_ctypes'
-sudo apt-fast install libbz2-dev -yqq # for resoluve ModuleNotFoundError: No module named '_bz2' in networkx
-sudo apt-fast install python3-pip -yqq
+yes | sudo apt-fast install libffi-dev -yq # for resolve ModuleNotFoundError: No module named '_ctypes'
+yes | sudo apt-fast install libbz2-dev -yq # for resoluve ModuleNotFoundError: No module named '_bz2' in networkx
+yes | sudo apt-fast install python3-pip -yq
 curl https://pyenv.run | bash
 #curl -sSL https://install.python-poetry.org | python3 -
-sudo apt-fast install python3-poetry -yqq
+yes | sudo apt-fast install python3-poetry -yq
 poetry config virtualenvs.in-project true
 poetry self add "poetry-dynamic-versioning[plugin]"
 pip3 install ruff-lsp # python formmter
 
 # docker
-# sudo apt-fast install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-sudo apt-fast install docker -yqq
+# yes | sudo apt-fast install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+yes | sudo apt-fast install docker -yq
 ## 権限設定 ref: https://linuxhandbook.com/docker-permission-denied/
 # ERROR: permission denied while trying to connect to the Docker daemon socket
-sudo groupadd docker
-sudo usermod -aG docker $USER
+yes | sudo groupadd docker
+yes | sudo usermod -aG docker $USER
 newgrp docker
 
 # etc
-sudo apt-fast install eog -yqq # preview image file
-sudo apt-fast install jq -yqq
+yes | sudo apt-fast install eog -yq # preview image file
+yes | sudo apt-fast install jq -yq
 
 # vim setup
-sudo apt-fast install neovim -yqq
+yes | sudo apt install neovim -yq
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugInstall -c q -c q
 
