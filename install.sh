a956a339182c3c59a856d7acded2ff7edb97a20d
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-27 00:41:35 +0900

    add yes

--- install.sh
+++ install.sh
@@ -7,48 +7,48 @@ sudo apt update -yq
 sudo apt upgrade -yq
 sudo apt install apt-fast -y
 # for ssh-agent
-sudo apt-fast install keychain -yq
+yes | sudo apt-fast install keychain -yq
 
 # space cli
-sudo apt-fast install zip unzip -yq
+yes | sudo apt-fast install zip unzip -yq
 curl -fsSL https://get.deta.dev/space-cli.sh | sh
 
-sudo apt-fast install tofrodos -yq # install fromdos, todos command
-sudo apt-fast install xsel xclip -yq # WSL2でクリップボードを有効にする
-sudo apt-fast install tig -yq
+yes | sudo apt-fast install tofrodos -yq # install fromdos, todos command
+yes | sudo apt-fast install xsel xclip -yq # WSL2でクリップボードを有効にする
+yes | sudo apt-fast install tig -yq
 
 # nodejs
-sudo apt-fast install npm -yq # node
-sudo npm install -g n
-sudo n stable
+yes | sudo apt-fast install npm -yq # node
+yes | sudo npm install -g n
+yes | sudo n stable
 sodo apt-fast purge nodejs npm -yq # n導入後は不要なため
 
 # python
-sudo apt-fast install libffi-dev -yq # for resolve ModuleNotFoundError: No module named '_ctypes'
-sudo apt-fast install libbz2-dev -yq # for resoluve ModuleNotFoundError: No module named '_bz2' in networkx
-sudo apt-fast install python3-pip -yq
+yes | sudo apt-fast install libffi-dev -yq # for resolve ModuleNotFoundError: No module named '_ctypes'
+yes | sudo apt-fast install libbz2-dev -yq # for resoluve ModuleNotFoundError: No module named '_bz2' in networkx
+yes | sudo apt-fast install python3-pip -yq
 curl https://pyenv.run | bash
 #curl -sSL https://install.python-poetry.org | python3 -
-sudo apt-fast install python3-poetry -yq
+yes | sudo apt-fast install python3-poetry -yq
 poetry config virtualenvs.in-project true
 poetry self add "poetry-dynamic-versioning[plugin]"
 pip3 install ruff-lsp # python formmter
 
 # docker
-# yes | sudo apt-fast install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-sudo apt-fast install docker -yq
+# yes | yes | sudo apt-fast install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+yes | sudo apt-fast install docker -yq
 ## 権限設定 ref: https://linuxhandbook.com/docker-permission-denied/
 # ERROR: permission denied while trying to connect to the Docker daemon socket
-sudo groupadd docker
-sudo usermod -aG docker $USER
+yes | sudo groupadd docker
+yes | sudo usermod -aG docker $USER
 newgrp docker
 
 # etc
-sudo apt-fast install eog -yq # preview image file
-sudo apt-fast install jq -yq
+yes | sudo apt-fast install eog -yq # preview image file
+yes | sudo apt-fast install jq -yq
 
 # vim setup
-sudo apt install neovim -yq
+yes | sudo apt install neovim -yq
 ./init_dots.sh # keychinに依存
 . ~/.bashrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
