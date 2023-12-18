d84f548e7e2a22fa895bdca8c85f0991eb6ef4b4
Shogo Goto <gotoadmn0605@gmail.com>
2023-12-18 23:17:01 +0900

    apt-get -> apt

--- install.sh
+++ install.sh
@@ -44,8 +44,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -yq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update
+sudo apt install -yq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo groupadd docker -f
 sudo gpasswd -a $USER docker
 
