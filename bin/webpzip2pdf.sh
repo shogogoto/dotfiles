a5c0e2ec8b03b5d73135183a6eb20ebff8614ba7
Shogo Goto <gotoadmn0605@gmail.com>
2024-08-15 19:22:33 +0900

    ファイル名の空白対応

--- bin/webpzip2pdf.sh
+++ bin/webpzip2pdf.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+find . -name "*.zip" |while read f
+do
+  new=`echo $f| sed 's/\s/_/g'`
+  mv "$f" $new
+done
 
 for p_zip in $@
 do
-  out_folder=`basename -s .zip $p_zip`
+  out_folder=`basename -s .zip "$p_zip"`
   out_folder2=`echo $out_folder | sed 's/\s/_/g'`
   mkdir ${out_folder2}
   unzip "${p_zip}" -d $out_folder2
