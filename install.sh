81b656483036541032d7bdd960091e90d2b6c5b4
Shogo Goto <gotoadmn0605@gmail.com>
2025-03-18 22:40:02 +0900

    luarocks for lazy.nvim

--- install.sh
+++ install.sh
@@ -67,6 +67,7 @@ sudo apt install cargo -yq # aiツール で必要
 sudo apt install silversearcher-ag -yq # fzf for vim
 sudo apt install cmigemo -yq
 sudo apt install ripgrep bat universal-ctags -yq # for fzf.vim
+sudo apt install luarocks -yq # for neovim lua and lazy.nvim
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
