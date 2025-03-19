d25349579e1a337cfd901a1598c685c517698972
Shogo Goto <gotoadmn0605@gmail.com>
2025-03-19 15:48:33 +0900

    install lazygit

--- install.sh
+++ install.sh
@@ -61,6 +61,13 @@ sudo apt install gh -yq # GitHub CLI
 sudo apt install hub -yq # cliからgithubを開く
 curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash # GitHub Actionsのローカル実行CLI
 
+## LazyGit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
+
 # vim setup
 sudo apt install neovim -yq
 sudo apt install cargo -yq # aiツール で必要
