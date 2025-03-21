7f3d2e4fdc6bf645048f92e269f72c4976309009
Shogo Goto <gotoadmn0605@gmail.com>
2025-03-21 17:12:14 +0900

    avanteに食わせるファイルをfilerから選択できるようにした

--- config/nvim/lua/plugins/filer.lua
+++ config/nvim/lua/plugins/filer.lua
@@ -27,7 +27,29 @@ return {
             ["s"] = "split_with_window_picker",
             ["v"] = "vsplit_with_window_picker",
             ["h"] = "close_node",
+            ['oa'] = 'avante_add_files',
           }
+        },
+        commands = {
+          avante_add_files = function(state)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local relative_path = require('avante.utils').relative_path(filepath)
+            local sidebar = require('avante').get()
+            local open = sidebar:is_open()
+            -- ensure avante sidebar is open
+            if not open then
+              require('avante.api').ask()
+              sidebar = require('avante').get()
+            end
+
+            sidebar.file_selector:add_selected_file(relative_path)
+
+            -- remove neo tree buffer
+            if not open then
+              sidebar.file_selector:remove_selected_file('neo-tree filesystem [1]')
+            end
+          end,
         },
       },
     },
@@ -36,38 +58,6 @@ return {
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
     end,
-    -- config = function()
-    --   require('neo-tree').setup({
-    --     filesystem = {
-    --       commands = {
-    --         avante_add_files = function(state)
-    --           local node = state.tree:get_node()
-    --           local filepath = node:get_id()
-    --           local relative_path = require('avante.utils').relative_path(filepath)
-    --           local sidebar = require('avante').get()
-    --           local open = sidebar:is_open()
-    --           -- ensure avante sidebar is open
-    --           if not open then
-    --             require('avante.api').ask()
-    --             sidebar = require('avante').get()
-    --           end
-
-    --           sidebar.file_selector:add_selected_file(relative_path)
-
-    --           -- remove neo tree buffer
-    --           if not open then
-    --             sidebar.file_selector:remove_selected_file('neo-tree filesystem [1]')
-    --           end
-    --         end,
-    --       },
-    --       window = {
-    --         mappings = {
-    --           ['oa'] = 'avante_add_files',
-    --         },
-    --       },
-    --     },
-    --   })
-    -- end,
   },
   {
     -- 既存の分割ウィンドウを指定してopen
