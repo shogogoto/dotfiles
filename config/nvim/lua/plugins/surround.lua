bf7f9dccff357b2385c5aa32b65eae673ec12a67
Shogo Goto <gotoadmn0605@gmail.com>
2025-03-22 22:14:01 +0900

    括弧移動

---
+++ config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,29 @@
+return {
+  -- nvim-matchup: 括弧や対応するキーワードの間をジャンプするためのプラグイン
+  {
+    "andymass/vim-matchup",
+    event = "VeryLazy",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      matchup = {
+        enable = true,              -- プラグインを有効化
+        disable = {},               -- 無効にする言語を指定
+        disable_virtual_text = false, -- 仮想テキストを有効化
+        include_match_words = true,  -- vim の matchit 互換機能を含める
+        treesitter = {
+          enable = true,           -- Treesitter統合を有効化
+          disable = {},            -- Treesitter統合を無効にする言語を指定
+        },
+      }
+    },
+    keys = {
+      { "%", desc = "Jump to matching pair" },
+      { "[%", desc = "Jump to previous matching pair" },
+      { "]%", desc = "Jump to next matching pair" },
+      { "g%", desc = "Jump to inside of matching pair" },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+}
