43903038665f328e72208caab89e059040620538
Shogo Goto <gotoadmn0605@gmail.com>
2025-03-23 22:51:29 +0900

    選択文字列でweb検索できる

--- config/nvim/lua/plugins/web-search.lua
+++ config/nvim/lua/plugins/web-search.lua
@@ -1,3 +1,22 @@
 return {
-  'voldikss/vim-browser-search'
+  "voldikss/vim-browser-search",
+  lazy = true,
+  keys = {
+    { "<leader>s", "<Plug>SearchNormal", desc = "Browser search (normal mode)" },
+    { "<leader>s", "<Plug>SearchVisual", mode = "v", desc = "Browser search (visual mode)" },
+  },
+  init = function()
+    vim.g.browser_search_default_engine = "google"
+    
+    -- 利用可能な検索エンジンを設定
+    vim.g.browser_search_engines = {
+      google = "https://www.google.com/search?q=%s",
+      github = "https://github.com/search?q=%s",
+      stackoverflow = "https://stackoverflow.com/search?q=%s",
+      duckduckgo = "https://duckduckgo.com/?q=%s",
+    }
+    
+    -- ブラウザパスを指定（オプション、自動検出される場合は不要）
+    -- vim.g.browser_search_browser_path = "/usr/bin/firefox"
+  end,
 }
