bff02d08aad49ab7055e7bba7fc580ff759b2af0
Shogo Goto <gotoadmn0605@gmail.com>
2025-03-24 14:47:21 +0900

    migemoでローマ字のまま日本語検索

--- bash_aliases
+++ bash_aliases
@@ -1,4 +1,4 @@
-alias vn="vi -u NONE -N" # 設定なしのデフォルトで起動
+alias vin="vi -u NONE -N" # 設定なしのデフォルトで起動
 alias dos2unix='fromdos'
 alias unix2dos='todos'
 alias wt='curl wttr.in'
--- config/nvim/lua/config.lua
+++ config/nvim/lua/config.lua
@@ -25,17 +25,27 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
-    { "vim-jp/vimdoc-ja", lazy = true, keys = {{ "h", mode = "c",},}, },
-    {
-      "ntk148v/habamax.nvim",
-      dependencies={ "rktjmp/lush.nvim" }
-    },
     { 'nacro90/numb.nvim', opts = {} },
     { "jghauser/mkdir.nvim" }, -- Vim起動時や:editしたときに存在しないディレクトリを指定すると勝手にmkdir
     { "tpope/vim-commentary" },
+    -- japanese
+    { "vim-jp/vimdoc-ja", lazy = true, keys = {{ "h", mode = "c",},}, },
+    { "sei40kr/migemo-search.nvim",
+      event = "CmdlineEnter",
+      config = function()
+        local ms = require("migemo-search")
+        ms.setup({
+          cmigemo_exec_path = "/usr/bin/cmigemo",
+          migemo_dict_path = "/usr/share/cmigemo/utf-8/migemo-dict",
+        })
+        vim.keymap.set("c", "<CR>", ms.cr)
+      end,
+    },
     -- text object
     { "tpope/vim-surround" },
     { "machakann/vim-sandwich" },
+    -- colorscheme
+    { "ntk148v/habamax.nvim", dependencies={ "rktjmp/lush.nvim" } },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
