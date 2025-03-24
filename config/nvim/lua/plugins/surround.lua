0d95593f23b37d0955b88455ee22f231e2067520
Shogo Goto <gotoadmn0605@gmail.com>
2025-03-24 21:07:17 +0900

    refactor

--- config/nvim/lua/plugins/surround.lua
+++ config/nvim/lua/plugins/surround.lua
@@ -1,23 +1,22 @@
 return {
-  -- nvim-matchup: 括弧や対応するキーワードの間をジャンプするためのプラグイン
   {
-    "andymass/vim-matchup",
+    "andymass/vim-matchup", -- 対へジャンプ
     event = "VeryLazy",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     opts = {
       matchup = {
-        enable = true,              -- プラグインを有効化
-        disable = {},               -- 無効にする言語を指定
+        enable = true,                -- プラグインを有効化
+        disable = {},                 -- 無効にする言語を指定
         disable_virtual_text = false, -- 仮想テキストを有効化
-        include_match_words = true,  -- vim の matchit 互換機能を含める
+        include_match_words = true,   -- vim の matchit 互換機能を含める
         treesitter = {
-          enable = true,           -- Treesitter統合を有効化
-          disable = {},            -- Treesitter統合を無効にする言語を指定
+          enable = true,              -- Treesitter統合を有効化
+          disable = {},               -- Treesitter統合を無効にする言語を指定
         },
-      }
+      },
     },
     keys = {
-      { "%", desc = "Jump to matching pair" },
+      { "%",  desc = "Jump to matching pair" },
       { "[%", desc = "Jump to previous matching pair" },
       { "]%", desc = "Jump to next matching pair" },
       { "g%", desc = "Jump to inside of matching pair" },
@@ -29,8 +28,15 @@ return {
   {
     "ysmb-wtsg/in-and-out.nvim",
     keys = {
-      { "<M-j>", function() require("in-and-out").in_and_out() end, mode = {"i", "n"} },
+      {
+        "<M-j>",
+        function()
+          require("in-and-out").in_and_out()
+        end,
+        mode = { "i", "n" },
+        desc = "対から出る/次の対に入る",
+      },
     },
     opts = { additional_targets = { "“", "”" } },
-  }
+  },
 }
