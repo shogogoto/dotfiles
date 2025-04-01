c0ffe7c8b7828462133476a4203c1252fc443ded
Shogo Goto <gotoadmn0605@gmail.com>
2025-04-01 13:38:29 +0900

    server(mason)の設定をformat.luaへ移動

--- config/nvim/lua/plugins/format.lua
+++ config/nvim/lua/plugins/format.lua
@@ -1,4 +1,4 @@
-local js_formatters = { "biome", "biome-check", "biome-organize-import" }
+local js_formatters = { "biome" }
 
 return {
 	{
@@ -13,7 +13,6 @@ return {
 					javascriptreact = js_formatters,
 					typescript = js_formatters,
 					typescriptreact = js_formatters,
-					markdown = js_formatters,
 					html = js_formatters,
 					css = js_formatters,
 					json = js_formatters,
@@ -65,6 +64,56 @@ return {
 		keys = {
 			{ "<leader>ws", "<cmd>StripWhitespace<CR>", desc = "空白文字を削除" },
 			{ "<leader>wt", "<cmd>ToggleWhitespace<CR>", desc = "空白文字のハイライト表示を切り替え" },
+		},
+	},
+	{
+		"williamboman/mason.nvim", -- LSPのインストール管理
+		opts = {
+			ui = {
+				icons = { package_installed = "✓", package_pending = "➜", package_uninstalled = "✗" },
+			},
+		},
+		keys = { { "<leader>m", "<cmd>Mason<CR>", desc = "Mason Package Manager Menu" } },
+		dependencies = {},
+	},
+	{ -- masonのdepsに入れたかったけどauto install 走らなくなった
+		-- 自動インストールどっちもできる
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		-- "williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				--lua
+				"stylua",
+				"lua-language-server", --"lua_ls",
+				"luacheck",
+				-- python
+				"pyright",
+				"ruff", -- formatter
+				"pyproject-flake8",
+				-- typescript
+				"typescript-language-server", --"ts_ls",
+				"biome",
+				-- "eslint-lsp",
+				-- "eslint_d",
+				-- "prettier",
+				"bash-language-server", --"bashls",
+				"dockerfile-language-server", --"dockerls",
+				"docker-compose-language-service", -- "docker_compose_language_service",
+				"cypher-language-server", --"cypher_ls",
+				"vim-language-server", --"vimls",
+				-- text
+				"json-lsp", -- "jsonls",
+				"yaml-language-server", --"yamlls",
+				-- markdown
+				-- "markdown-toc",
+				-- "markdownlint",
+
+				"cspell",
+				"copilot-language-server",
+			},
+			auto_update = true,
+			run_on_start = true,
+			-- automatic_installation = true,
 		},
 	},
 }
