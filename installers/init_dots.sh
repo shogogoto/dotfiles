ba8e548f9f63c0ed240d5e5d20f85125a8e6de0b
Shogo Goto <gotoadmn0605@gmail.com>
2025-05-12 18:11:39 +0900

    fix add files

---
+++ installers/init_dots.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dot_files=`cat << EOS
+gitconfig
+tigrc
+EOS
+`
+
+for file in $dot_files
+do
+  ln -is ~/dotfiles/$file $HOME/.$file
+done
+
+source_bash=". ~/dotfiles/bashrc"
+grep -q "$source_bash" ~/.bashrc
+
+# マッチあり
+if [ $? = 0 ]; then
+  echo "already written source dotfiles bashrc"
+else
+  echo $source_bash >> ~/.bashrc
+  echo "eval "$(starship init bash)"" >> ~/.bashrc
+  echo "added source dotfiles bashrc"
+fi
