56764a83fb725b4f091abb66ea883354081370ad
Shogo Goto <gotoadmn0605@gmail.com>
2025-08-17 15:01:48 +0900

    update

--- config/nvim/lua/plugins/auto-pair.lua
+++ config/nvim/lua/plugins/auto-pair.lua
@@ -1,21 +1,23 @@
+-- NOTE: このファイルは使用しません。tree-sitter.luaに統合されました。
+-- 以下は参照用に残しています。
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	config = function()
-		local npairs = require("nvim-autopairs")
-		
-		npairs.setup({
-			check_ts = true,
-			ts_config = {
-				javascript = { "template_string" },
-				typescript = { "template_string" },
-				typescriptreact = { "template_string" }
-			}
-		})
-
-		-- cmpとの連携
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		local cmp = require("cmp")
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-	end
+	-- "windwp/nvim-autopairs",
+	-- event = "InsertEnter",
+	-- config = function()
+	-- 	local npairs = require("nvim-autopairs")
+	--
+	-- 	npairs.setup({
+	-- 		check_ts = true,
+	-- 		ts_config = {
+	-- 			javascript = { "template_string" },
+	-- 			typescript = { "template_string" },
+	-- 			typescriptreact = { "template_string" }
+	-- 		}
+	-- 	})
+	--
+	-- 	-- cmpとの連携
+	-- 	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+	-- 	local cmp = require("cmp")
+	-- 	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	-- end
 }
