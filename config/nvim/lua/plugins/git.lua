c9597a56414b39c99fc450e3441bd73079c993a4
Shogo Goto <gotoadmn0605@gmail.com>
2026-07-04 22:19:20 +0900

    install git-timemachine and bookmarks

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,91 +1,98 @@
 return {
-  { "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    }
-  },
-  { "codeindulgence/vim-tig",
-    keys = {
-        { "<leader>gg", "<cmd>tabnew | Tig<cr>", desc = "Open Tig in new tab" }
-    }
-  },
-  { "lewis6991/gitsigns.nvim",
-    keys = {
-      { "]g", "<cmd>Gitsigns next_hunk<CR>", desc = "Next Git diff hunk" },
-      { "[g", "<cmd>Gitsigns prev_hunk<CR>", desc = "Previous Git diff hunk" },
-      { "sg", "<cmd>Gitsigns preview_hunk<CR>", desc = "Show Git diff hunk" },
-    },
-    opts = {
-      signs = {
-        add          = { text = '┃' },
-        change       = { text = '┃' },
-        delete       = { text = '_' },
-        topdelete    = { text = '‾' },
-        changedelete = { text = '~' },
-        untracked    = { text = '┆' },
-      },
-      signs_staged = {
-        add          = { text = '┃' },
-        change       = { text = '┃' },
-        delete       = { text = '_' },
-        topdelete    = { text = '‾' },
-        changedelete = { text = '~' },
-        untracked    = { text = '┆' },
-      },
-      signs_staged_enable = true,
-      signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-      numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-      linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-      word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-      watch_gitdir = {
-        follow_files = true
-      },
-      auto_attach = true,
-      attach_to_untracked = false,
-      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-      current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
-        ignore_whitespace = false,
-        virt_text_priority = 100,
-        use_focus = true,
-      },
-      current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-      sign_priority = 6,
-      update_debounce = 100,
-      status_formatter = nil, -- Use default
-      max_file_length = 40000, -- Disable if file is longer than this (in lines)
-      preview_config = {
-        -- Options passed to nvim_open_win
-        border = 'single',
-        style = 'minimal',
-        relative = 'cursor',
-        row = 0,
-        col = 1
-      },
-    },
-  },
-  { 'tpope/vim-fugitive'}, -- :GitXX コマンドの追加
-  { "sindrets/diffview.nvim", opts = {},
-    keys = {
-      { "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Open DiffView" },
-      { "<leader>gD", "<cmd>DiffviewClose<CR>", desc = "Close DiffView" },
-    },
-  },
-
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+	{
+		"codeindulgence/vim-tig",
+		keys = {
+			{ "<leader>gg", "<cmd>tabnew | Tig<cr>", desc = "Open Tig in new tab" },
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		keys = {
+			{ "]g", "<cmd>Gitsigns next_hunk<CR>", desc = "Next Git diff hunk" },
+			{ "[g", "<cmd>Gitsigns prev_hunk<CR>", desc = "Previous Git diff hunk" },
+			{ "sg", "<cmd>Gitsigns preview_hunk<CR>", desc = "Show Git diff hunk" },
+		},
+		opts = {
+			signs = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged_enable = true,
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+			watch_gitdir = {
+				follow_files = true,
+			},
+			auto_attach = true,
+			attach_to_untracked = false,
+			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 1000,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = true,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil, -- Use default
+			max_file_length = 40000, -- Disable if file is longer than this (in lines)
+			preview_config = {
+				-- Options passed to nvim_open_win
+				border = "single",
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
+		},
+	},
+	{ "tpope/vim-fugitive" }, -- :GitXX コマンドの追加
+	{
+		"sindrets/diffview.nvim",
+		opts = {},
+		keys = {
+			{ "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Open DiffView" },
+			{ "<leader>gD", "<cmd>DiffviewClose<CR>", desc = "Close DiffView" },
+		},
+	},
+	{
+		"dallagi/git-timemachine.nvim",
+	},
 }
