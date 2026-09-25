55152112382702415ef58c120cda43ff65ac726c
Shogo Goto <gotoadmn0605@gmail.com>
2026-09-25 11:42:15 +0900

    wezterm bell

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,4 +8,5 @@ config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 -- config.hide_tab_bar_if_only_one_tab = true -- タブ1つの時にタブバーを隠す
 config.window_decorations = "NONE" -- タイトルバー削除
 -- config.enable_tab_bar = false -- 2. タブバーを有効にする (統合先として必要)
+config.notification_handling = "AlwaysShow"
 return config
